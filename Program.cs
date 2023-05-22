f3593a54da9856791294bb58e4c770c6d1da18f9
Julia <maksulyax^2gmail.com>
2023-05-22 13:39:09 +0300

    HW

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Ведите число: ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Ведите число: ");
+
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num < num2)
+// {
+//     Console.WriteLine($"{num} меньше, {num2} больше");
+// }
+// if (num > num2)
+// {
+//     Console.WriteLine($"{num2} меньше, {num} больше");
+// }
+
+
+
+
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Ведите число: ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Ведите число: ");
+
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Ведите число: ");
+
+// int num3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = num;
+
+//  if (max < num2)
+//  {
+//     max = num2;
+//  }
+//  if (max < num3)
+//  {
+//  max = num3;
+//  }
+
+//  Console.WriteLine(max);
+
+
+
+
+
+
+//  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Ведите число: ");
+
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num%2 == 0)
+// {
+//     Console.WriteLine($"число {num} является четным");
+// }
+// if (num%2 != 0)
+// {
+//     Console.WriteLine($"число {num} не является четным");
+// }
+
+
+
+
+// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Console.WriteLine("Ведите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// while (num > 0)
+// {
+//     if (num%2==0)
+//     {
+//         Console.WriteLine(num);
+//         num = num -2;
+//     }
+//     if (num%2!=0)
+//     {
+//         num = num - 1;
+//         Console.WriteLine(num);
+//         num = num -2;
+//     }
+// }
